850d8a50a7176ebe12b06d5a3fbb2f3e2798e6fe
njdrewett <njdrewett@sky.com>
2023-03-21 20:39:42 +0000

    Initial Ci/CD setup

--- ec2.tf
+++ ec2.tf
@@ -24,7 +24,7 @@ resource "aws_launch_configuration" "linux_launch_config" {
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data                   = <<EOF
+  user_data                   = <<-EOF
   #!/bin/bash
   echo "Hello World" > index.html
   nohup busybox httpd -f -p ${var.server_port} &
